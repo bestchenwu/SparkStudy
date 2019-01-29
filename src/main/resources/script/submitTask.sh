90477c9334b78756c1d7e43a9b6f460583f2b3a9
bestchenwu <bestchenwu@163.com>
2019-01-29 23:15:18 +0800

    修复checkpoint

--- src/main/resources/script/submitTask.sh
+++ src/main/resources/script/submitTask.sh
@@ -7,4 +7,4 @@
   --class com.spark.scala.sparkStreaming.checkpoint.CheckPointTestTask \
   --total-executor-cores 2 \
   --executor-memory 512m \
-  /data/StudySpace/SparkStudy/target/spark-1.0-SNAPSHOT-jar-with-dependencies.jar >submit.log 2>&1 &
+  /data/StudySpace/SparkStudy/target/spark-1.0-SNAPSHOT-jar-with-dependencies.jar hdfs://localhost:9000/usr/spark/checkpoint >submit.log 2>&1 &
