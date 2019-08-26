59202b107ffab4e8969fb0ec46b9762befe42ef6
bestchenwu <bestchenwu@163.com>
2019-08-26 23:00:36 +0800

    提交flink watermark学习

--- src/main/scala/com/flink/scala/source/SeqScalaTest.scala
+++ src/main/scala/com/flink/scala/source/SeqScalaTest.scala
@@ -10,7 +10,8 @@ import org.apache.flink.streaming.api.scala._
   */
 object SeqScalaTest {
   def main(args: Array[String]): Unit = {
-    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    //val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val env = StreamExecutionEnvironment.createLocalEnvironment()
     val longStream = env.generateSequence(1l,100l).filter(_%7l==0).map(_*2)
     longStream.print()
     env.execute("SeqScalaTest")
