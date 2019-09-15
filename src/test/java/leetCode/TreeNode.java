d4d358d3df3baaf8f10cb94b5157d8fccf1218c4
bestchenwu <bestchenwu@163.com>
2019-09-15 23:14:27 +0800

    提交

---
+++ src/test/java/leetCode/TreeNode.java
@@ -0,0 +1,22 @@
+package leetCode;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+    public TreeNode(int val,TreeNode left,TreeNode right){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return "TreeNode[val="+val+"]";
+    }
+}
