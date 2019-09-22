c7de305dd09c18ab47419c212a2d46fc9d4f3e94
bestchenwu <bestchenwu@163.com>
2019-09-22 18:19:09 +0800

    提交异步IO

--- src/main/scala/com/flink/scala/AsyncIO.scala
+++ src/main/scala/com/flink/scala/AsyncIO.scala
@@ -1,7 +1,10 @@
 package com.flink.scala
 
 /**
-  * https://www.cnblogs.com/Springmoon-venn/p/11103558.html
+  * 原文:https://www.cnblogs.com/Springmoon-venn/p/11103558.html
+  * 官方文档:https://flink.sojb.cn/dev/stream/operators/asyncio.html
+  *
+  * @author chenwu on 2019.9.22
   */
 class AsyncIO {
 
