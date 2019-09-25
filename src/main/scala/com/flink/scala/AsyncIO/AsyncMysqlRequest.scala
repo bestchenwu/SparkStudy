02a0be5087abdbcc9aaf2dac11ca4aab4049a94f
bestchenwu <bestchenwu@163.com>
2019-09-25 23:53:56 +0800

    提交异步读写

--- src/main/scala/com/flink/scala/AsyncIO/AsyncMysqlRequest.scala
+++ src/main/scala/com/flink/scala/AsyncIO/AsyncMysqlRequest.scala
@@ -1,11 +1,16 @@
 package com.flink.scala.AsyncIO
 
 import java.util.Properties
+import java.util.concurrent.TimeUnit
 
+import com.mysql.MysqlClient
+import com.spark.constants.SymbolConstants
 import org.apache.flink.api.common.serialization.SimpleStringSchema
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010
 import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.scala.async.{AsyncFunction, ResultFuture}
+
+import scala.concurrent.duration.TimeUnit
 /**
   * 原文:https://www.cnblogs.com/Springmoon-venn/p/11103558.html
   * 官方文档:https://flink.sojb.cn/dev/stream/operators/asyncio.html
@@ -14,12 +19,30 @@ import org.apache.flink.streaming.api.scala._
   */
 object AsyncMysqlRequest {
 
+  case class User(id:Long,userName:String,password:String="")
+
+  class AysncDataBaseRequest extends AsyncFunction[User,User]{
+
+    val mysqlClient:MysqlClient = MysqlClient()
+
+    override def asyncInvoke(input: User, resultFuture: ResultFuture[User]): Unit = {
+        val output = mysqlClient.querySingleObjectById("User",Array("password"),input.id)
+        val user = User(input.id,input.userName,output(0))
+        resultFuture.complete(List(user))
+    }
+  }
+
   def main(array:Array[String]): Unit ={
       val env = StreamExecutionEnvironment.getExecutionEnvironment
       val properties = new Properties()
-    properties.setProperty("bootstrap.servers", "localhost:9092")
-    properties.setProperty("group.id", "test-flink")
+      properties.setProperty("bootstrap.servers", "localhost:9092")
+      properties.setProperty("group.id", "test-flink")
       val kafkaStream = env.addSource(new FlinkKafkaConsumer010[String]("test-010",SimpleStringSchema,properties))
-      kafkaStream.flatMap(_.split("\\W+"))
+      val input = kafkaStream.flatMap(_.split("\\s+")).map(item=>{
+          val array = item.split(SymbolConstants.SYMBOL_DH)
+          val user = User(array(0).toLong,array(1))
+          user
+      })
+      AsyncDataStream.unorderedWait(input,new AysncDataBaseRequest(),10,TimeUnit.SECONDS,)
   }
 }
---
+++ src/main/scala/com/mysql/MysqlClient.scala
@@ -0,0 +1,96 @@
+package com.mysql
+
+import java.sql.{Connection, DriverManager, ResultSet, Statement}
+
+import com.spark.constants.SymbolConstants
+
+import scala.collection.mutable.ArrayBuffer
+
+/**
+  * mysql的client
+  *
+  * @param host
+  * @param port
+  * @param username
+  * @param password
+  * @param databaseName
+  */
+class MysqlClient(host:String="localhost",port:Int=3306,username:String,password:String,databaseName:String) {
+
+  private val connectionUrl={
+    try{
+      Class.forName("com.mysql.cj.jdbc.Driver")
+      "jdbc:mysql://"+host+":"+port+"/"+databaseName
+    }catch {
+      case e=>throw new IllegalStateException(e)
+    }
+  }
+
+  /**
+    * 获取mysql链接
+    *
+    * @return Connection
+    * @author chenwu on 2019.9.25
+    */
+  private def getConnection:Connection={
+    val connection = DriverManager.getConnection(connectionUrl)
+    connection
+  }
+
+  def querySingleObjectById(tableName:String,columns:Array[String],id:Long): Array[String] ={
+      val connection = getConnection
+
+      val sql =s"select ${columns.mkString(SymbolConstants.SYMBOL_DH)} from ${tableName} where id=${id}"
+      val statement = connection.createStatement();
+      val resultSet = statement.executeQuery(sql)
+      val array = ArrayBuffer[String]()
+      if(resultSet!=null && resultSet.first()){
+          for(column<-columns){
+              val value = resultSet.getString(column)
+              if(value==null||value.isEmpty){
+                  array.append(SymbolConstants.NAString)
+              }else{
+                  array.append(value)
+              }
+          }
+      }
+      quietlyCloseConnection(connection,statement,resultSet)
+      array.toArray
+  }
+
+  /**
+    * 关闭数据库链接等
+    *
+    * @param connection
+    * @param statement
+    * @param resultSet
+    * @author chenwu on 2019.9.25
+    */
+  private def quietlyCloseConnection(connection:Connection,statement:Statement,resultSet:ResultSet=null)={
+    if(resultSet!=null){
+      try{
+        resultSet.close()
+      }catch{
+        //ignore the exception
+        case e=>
+      }
+    }
+    if(statement!=null){
+      try{
+        statement.close()
+      }catch{
+        case e=>
+      }
+    }
+    if(connection!=null){
+      try{
+        connection.close()
+      }catch{
+        case e=>
+      }
+    }
+  }
+}
+object MysqlClient{
+
+}
