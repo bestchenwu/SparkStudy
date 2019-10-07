e3c0d6d36aa263691b8f3e0255501e49d4d28559
bestchenwu <bestchenwu@163.com>
2019-10-07 20:06:59 +0800

    提交stream学习

---
+++ src/main/java/com/java8Study/unit4_Stream/Dish.java
@@ -0,0 +1,69 @@
+package com.java8Study.unit4_Stream;
+
+/**
+ * 菜肴
+ *
+ * @author chenwu on 2019.10.7
+ */
+public class Dish {
+
+    private String name;
+    private boolean vegetarian;
+    private int calories;
+    private Type type;
+
+    public static Dish of(String name, boolean vegetarian, int calories, Type type) {
+        Dish dish = new Dish();
+        dish.setName(name);
+        dish.setVegetarian(vegetarian);
+        dish.setCalories(calories);
+        dish.setType(type);
+        return dish;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isVegetarian() {
+        return vegetarian;
+    }
+
+    public void setVegetarian(boolean vegetarian) {
+        this.vegetarian = vegetarian;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Dish{" +
+                "name='" + name + '\'' +
+                ", vegetarian=" + vegetarian +
+                ", calories=" + calories +
+                ", type=" + type +
+                '}';
+    }
+}
+
+enum Type {
+    MEAT, FISH, OTHER;
+}
---
+++ src/main/java/com/java8Study/unit4_Stream/StreamTest.java
@@ -0,0 +1,44 @@
+package com.java8Study.unit4_Stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+/**
+ * 使用JAVA流式运算,Stream类似于scala中的stream,表示流式计算<br/>
+ * 流式计算用collect触发计算操作
+ *
+ * @author chenwu on 2019.10.7
+ */
+public class StreamTest {
+
+    public static void main(String[] args){
+        List<Dish> dishList = new ArrayList<Dish>();
+        dishList.add(Dish.of("pork",false,800,Type.MEAT));
+        dishList.add(Dish.of("chicken",false,400,Type.MEAT));
+        dishList.add(Dish.of("french",true,530,Type.OTHER));
+        dishList.add(Dish.of("salmon",false,450,Type.FISH));
+        //以下是使用lambda表达式
+        List<String> resultList = dishList.stream().filter((Dish dish)->dish.isVegetarian())
+                .map((Dish dish)->dish.getName())
+                .limit(3)
+                .collect(toList());
+        //也可以使用函数值的方式
+        List<String> resultList1 = dishList.stream().filter(Dish::isVegetarian)
+                .map(Dish::getName)
+                .limit(3)
+                .collect(toList());
+        System.out.println(resultList);
+        System.out.println(resultList1);
+        //流只能被遍历一次,如果要遍历第二次,则需要重新从源头建立流
+        Stream<String> stream = resultList1.stream();
+        stream.forEach((String item)->System.out.println(item));
+        //在执行第二遍的时候会报stream has already been operated upon or closed的错误
+        //stream.forEach(System.out::println);
+        dishList.stream().filter((Dish dish)->{
+            System.out.println("filter:"+dish);
+            return dish.getCalories()>300;
+        });
+    }
+}
