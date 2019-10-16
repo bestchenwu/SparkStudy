d46ff4c0d9b5a69b3e38d34d269684637119f439
chenwu <chenwu@meitunmama.com>
2019-10-16 20:31:36 +0800

    提交leetcode234 283

---
+++ src/test/java/leetCode/LeetCode234.java
@@ -0,0 +1,50 @@
+package leetCode;
+
+/**
+ * https://leetcode-cn.com/problems/palindrome-linked-list
+ */
+public class LeetCode234 {
+
+    public boolean isPalindrome(ListNode head) {
+        if(head==null||head.next==null){
+            return false;
+        }
+        //计算链表的长度
+        int len = 0;
+        ListNode tmp = head;
+        while(tmp!=null){
+            len+=1;
+            tmp = tmp.next;
+        }
+        //反转前半段链表
+        ListNode preRoot = null;//前半段链表的头节点
+        ListNode preLast = null;//前半段链表的尾结点
+        tmp = head;//将指针仍然指向头节点
+        for(int i=0;i<len/2;i++){
+            preRoot = new ListNode(tmp.val);
+            preRoot .next = preLast;
+            preLast = preRoot;
+            tmp = tmp.next;
+        }
+        //如果是奇数个数的链表,则后半段链表的头节点需要向后挪一位
+        ListNode afterRoot = tmp;
+        if(len%2==1){
+            afterRoot = tmp.next;
+        }
+        while(preRoot!=null&&afterRoot!=null){
+            if(preRoot.val!=afterRoot.val){
+                return false;
+            }
+            preRoot = preRoot.next;
+            afterRoot = afterRoot.next;
+        }
+        return afterRoot==null&&preRoot==null;
+    }
+
+    public static void main(String[] args){
+        LeetCode234 leetCode234 = new LeetCode234();
+        ListNode node = new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(2,new ListNode(1)))));
+        boolean result = leetCode234.isPalindrome(node);
+        System.out.println(result);
+    }
+}
