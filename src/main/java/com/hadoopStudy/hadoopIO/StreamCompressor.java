84d2b5be5ed18070814cdda81d8632086fb0828f
bestchenwu <bestchenwu@163.com>
2019-10-17 08:53:51 +0800

    提交codeCpool

--- src/main/java/com/hadoopStudy/hadoopIO/StreamCompressor.java
+++ src/main/java/com/hadoopStudy/hadoopIO/StreamCompressor.java
@@ -2,9 +2,7 @@ package com.hadoopStudy.hadoopIO;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.IOUtils;
-import org.apache.hadoop.io.compress.CompressionCodec;
-import org.apache.hadoop.io.compress.CompressionInputStream;
-import org.apache.hadoop.io.compress.CompressionOutputStream;
+import org.apache.hadoop.io.compress.*;
 import org.apache.hadoop.util.ReflectionUtils;
 
 import java.io.File;
@@ -21,12 +19,23 @@ public class StreamCompressor {
         Class<?> clazz = Class.forName(compressCodeClassName);
         Configuration conf = new Configuration();
         CompressionCodec compressionCodec = (CompressionCodec)ReflectionUtils.newInstance(clazz, conf);
-        FileInputStream fis = new FileInputStream("D:\\logs\\test_driver_log.txt");
-        File file = new File("D:\\logs\\test_driver_log_compress.txt");
-        FileOutputStream fos = new FileOutputStream(file,false);
-        CompressionOutputStream outputStream = compressionCodec.createOutputStream(fos);
-        IOUtils.copyBytes(fis,outputStream,2046,true);
-        CompressionInputStream compressInputStream = compressionCodec.createInputStream(new FileInputStream("D:\\logs\\test_driver_log_compress.txt"));
-        IOUtils.copyBytes(compressInputStream,System.out,2046,true);
+        FileInputStream fis = new FileInputStream("/data/problem/brand.txt");
+        File file = new File("/data/problem/brand_compress.txt");
+        FileOutputStream fos = new FileOutputStream(file,true);
+        //这是使用自己创建一个压缩器的方式
+        //CompressionOutputStream outputStream = compressionCodec.createOutputStream(fos);
+        //IOUtils.copyBytes(fis,outputStream,2046,true);
+        //以下是通过压缩池来创建一个压缩器
+        Compressor compressor = null;
+        try{
+            compressor = CodecPool.getCompressor(compressionCodec);
+            CompressionOutputStream outputStream1 = compressionCodec.createOutputStream(fos, compressor);
+            IOUtils.copyBytes(fis,outputStream1,2046,false);
+            outputStream1.finish();
+        }finally {
+            CodecPool.returnCompressor(compressor);
+        }
+        //CompressionInputStream compressInputStream = compressionCodec.createInputStream(new FileInputStream("D:\\logs\\test_driver_log_compress.txt"));
+        //IOUtils.copyBytes(compressInputStream,System.out,2046,true);
     }
 }
