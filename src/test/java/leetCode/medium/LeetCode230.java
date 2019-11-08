a19a0de0744bd8221e5a9e50c5dff71af77c0412
chenwu <chenwu@meitunmama.com>
2019-11-08 19:06:32 +0800

    提交leetcode

--- src/test/java/leetCode/medium/LeetCode230.java
+++ src/test/java/leetCode/medium/LeetCode230.java
@@ -2,45 +2,73 @@ package leetCode.medium;
 
 import leetCode.TreeNode;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst/
- *
+ * <p>
  * 给定一个二叉搜索树，编写一个函数 kthSmallest 来查找其中第 k 个最小的元素。
- *
+ * <p>
  * 输入: root = [3,1,4,null,2], k = 1
- *    3
- *   / \
- *  1   4
- *   \
- *    2
+ * 3
+ * / \
+ * 1   4
+ * \
+ * 2
  * 输出: 1
  */
 public class LeetCode230 {
 
-    private TreeNode kth_find(TreeNode root,int index,int k){
-        if(root==null){
+    private Integer kth_find(TreeNode root, AtomicInteger index, int k) {
+        Integer result = null;
+        if (root == null) {
             return null;
+        } else {
+            index.addAndGet(1);
         }
-        TreeNode leftNode = kth_find(root.left,index,k);
-        if(index == k&&leftNode!=null){
-            return leftNode;
+        if(index.get()==k){
+            result = root.val;
+            return result;
         }
-        if(k-index==1){
-            return root;
+        result = kth_find(root.left, index, k);
+        if(result!=null){
+            return result;
         }
-        if(leftNode!=null){
-            index+=2;
-        }else{
-            index+=1;
+        if(index.get()==k){
+            return root.val;
         }
-        TreeNode rightNode = kth_find(root.right,index,k);
-
-        return rightNode;
+        result  = kth_find(root.right, index, k);
+        if(result!=null){
+            return result;
+        }
+        return null;
     }
 
     public int kthSmallest(TreeNode root, int k) {
         //左根右遍历
         //如果左边节点找到了,index+1,直到index = k
-        return 0;
+        if(root==null){
+            return -1;
+        }
+        AtomicInteger index = new AtomicInteger(0);
+        Integer result = null;
+        result = kth_find(root.left,index,k);
+        if(result !=null){
+            return result.intValue();
+        }
+        //由于判断了root非空,所以这里index需要增加1
+        index.addAndGet(1);
+        if(index.get()==k){
+            return root.val;
+        }
+        result = kth_find(root.right,index,k);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        LeetCode230 leetCode230 = new LeetCode230();
+        TreeNode root = new TreeNode(3, new TreeNode(1, null, new TreeNode(2)), new TreeNode(4));
+        int result = leetCode230.kthSmallest(root, 4);
+        System.out.println(result);
     }
 }
