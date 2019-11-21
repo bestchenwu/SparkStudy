1d68fff032775df6c7281a1f3e19160e1e8946a8
chenwu <chenwu@meitunmama.com>
2019-11-21 11:33:22 +0800

    提交watermark

---
+++ src/main/scala/com/flink/scala2/watermark/CreateWaterMarkTest.scala
@@ -0,0 +1,12 @@
+package com.flink.scala2.watermark
+
+/**
+  * 生成时间戳
+  */
+class CreateWaterMarkTest {
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+}
--- src/main/scala/com/scalaStudy/unit1_string/StringTest.scala
+++ src/main/scala/com/scalaStudy/unit1_string/StringTest.scala
@@ -2,6 +2,6 @@ package com.scalaStudy.unit1_string
 
 object StringTest extends App{
 
-    val str = "hello,world"
-    print(str.length)
+    val str = "scala"
+    print(str.take(2).drop(2).capitalize)
 }
