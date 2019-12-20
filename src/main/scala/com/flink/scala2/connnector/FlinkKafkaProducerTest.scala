416bd4a56697b53f9a87d97f432950359bf85e8e
chenwu <chenwu@meitunmama.com>
2019-12-20 18:32:24 +0800

    提交flink

--- src/main/scala/com/flink/scala2/connnector/FlinkKafkaProducerTest.scala
+++ src/main/scala/com/flink/scala2/connnector/FlinkKafkaProducerTest.scala
@@ -3,6 +3,7 @@ package com.flink.scala2.connnector
 import org.apache.flink.api.common.serialization.SimpleStringSchema
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer
+import org.apache.flink.streaming.api.scala.createTypeInformation
 
 /**
   * 写入消息到kafka的测试类
@@ -13,7 +14,7 @@ object FlinkKafkaProducerTest {
 
   def main(args: Array[String]): Unit = {
       val env = StreamExecutionEnvironment.createLocalEnvironment(1)
-      env.fromCollection(Seq())
+      val dataStream = env.fromCollection(Seq())
       val kafkaProducer = new FlinkKafkaProducer[String]("127.0.0.1:9092","test-210",new SimpleStringSchema())
 
   }
