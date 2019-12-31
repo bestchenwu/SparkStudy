ff8545e8752bdc21a293b92cf45fc4cc6dfb3f11
chenwu <chenwu@meitunmama.com>
2019-12-31 20:27:34 +0800

    提交flink 批处理容错性代码

---
+++ src/main/scala/com/flink/scala2/dataset/Tolerance/ToleranceTest.scala
@@ -0,0 +1,17 @@
+package com.flink.scala2.dataset.Tolerance
+
+import org.apache.flink.api.common.restartstrategy.RestartStrategies.FixedDelayRestartStrategyConfiguration
+import org.apache.flink.api.scala.ExecutionEnvironment
+
+/**
+  * 容错
+  */
+object ToleranceTest {
+
+  def main(args: Array[String]): Unit = {
+    val env = ExecutionEnvironment.getExecutionEnvironment
+    //env.setNumberOfExecutionRetries(3)
+    //重试3次,重试间隔是5000毫秒
+    env.setRestartStrategy(new FixedDelayRestartStrategyConfiguration(3,5000))
+  }
+}
