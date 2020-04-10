48bba4c0f0a1cf411115a57243b0e2ff213644cb
bestchenwu <bestchenwu@163.com>
2020-04-10 20:15:33 +0800

    提交leetcode160 求链表的闭环

--- src/test/java/leetCode/simple/LeetCode160.java
+++ src/test/java/leetCode/simple/LeetCode160.java
@@ -6,7 +6,22 @@ import leetCode.ListNode;
  * https://leetcode-cn.com/problems/intersection-of-two-linked-lists/submissions/
  */
 public class LeetCode160 {
+
+
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null){
+            return null;
+        }
+        ListNode pa = headA;
+        ListNode pb = headB;
+        while(pa!=pb){
+            pa = pa!=null?pa.next:headB;
+            pb = pb!=null?pb.next:headA;
+        }
+        return pa;
+    }
+
+    public ListNode getIntersectionNode0(ListNode headA, ListNode headB) {
         if(headA==null&&headB==null){
             return null;
         }
@@ -23,5 +38,16 @@ public class LeetCode160 {
             pheadB = pheadB!=null?pheadB.next:headA;
         }
         return pheadA;
+    }
+
+    public static void main(String[] args) {
+        LeetCode160 leetCode160 = new LeetCode160();
+//        ListNode listNodeA = new ListNode(4,new ListNode(1,new ListNode(8,new ListNode(4,new ListNode(5)))));
+//        ListNode listNodeB = new ListNode(5,new ListNode(0,new ListNode(1,new ListNode(8,new ListNode(4,new ListNode(5))))));
+        ListNode listNodeA = new ListNode(2,new ListNode(6,new ListNode(4)));
+        ListNode listNodeB = new ListNode(1,new ListNode(5));
+
+        ListNode intersectionNode = leetCode160.getIntersectionNode(listNodeA, listNodeB);
+        System.out.println("val:"+intersectionNode.val);
     }
 }
