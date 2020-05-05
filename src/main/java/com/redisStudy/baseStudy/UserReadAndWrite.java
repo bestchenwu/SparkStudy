e98980c2d359f126baf8db3215a277ec0a1df102
bestchenwu <bestchenwu@163.com>
2020-05-05 22:43:59 +0800

    提交<Redis深度历险> 第一章万丈高楼平地起

--- src/main/java/com/redisStudy/baseStudy/UserReadAndWrite.java
+++ src/main/java/com/redisStudy/baseStudy/UserReadAndWrite.java
@@ -1,16 +1,86 @@
 package com.redisStudy.baseStudy;
 
+import com.alibaba.fastjson.JSON;
+import com.redisStudy.client.CommonRedissionClient;
+import com.redisStudy.model.User;
+import org.apache.commons.lang.StringUtils;
+import org.junit.Assert;
+
+import java.lang.reflect.Field;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 测试{@link User}的redis读写
+ *
+ * @author chenwu on 2020.5.5
+ */
 public class UserReadAndWrite {
 
-    public void WritePeopleInJson(){
+    private CommonRedissionClient commonRedissionClient = CommonRedissionClient.getInstance("redis/local_redis.properties");
+
+    public void writePeopleInJson(String key,User user){
+        if(user==null){
+            return;
+        }
+        String jsonString = JSON.toJSONString(user);
+        commonRedissionClient.setSingleKey(key,jsonString,10l, TimeUnit.MINUTES,String.class);
+    }
+
+    public User readPeopleFromJson(String key){
+        if(StringUtils.isBlank(key)){
+            return null;
+        }
+        String value = commonRedissionClient.getSingleKey(key,String.class);
+        if(StringUtils.isBlank(value)){
+            return null;
+        }
+        User user = JSON.parseObject(value, User.class);
+        return user;
+    }
+
+    public void writeUserInHash(String key,User user){
+        if(StringUtils.isBlank(key) || user==null){
+            return;
+        }
+        Field[] declaredFields = user.getClass().getDeclaredFields();
+        for(Field field:declaredFields){
+            field.setAccessible(true);
+            try{
+                Object object = field.get(user);
+                commonRedissionClient.hsetSingleKey(key,field.getName(),object,10,TimeUnit.MINUTES);
+            }catch(Exception e){
+                throw new RuntimeException(e);
+            }
+        }
 
     }
 
-    public void ReadPeopleFromJson(){
-
+    public <T> T readUserInHash(String key,String field,Class<T> valueClass){
+        if(StringUtils.isBlank(key) || StringUtils.isBlank(field)){
+            return null;
+        }
+        T t = commonRedissionClient.hgetSingleKey(key, field, valueClass);
+        return t;
     }
 
     public static void main(String[] args) {
-
+        UserReadAndWrite userReadAndWrite = new UserReadAndWrite();
+        //String key = "testUser";
+        //字典的方式
+        String key = "testHash";
+        User user = new User();
+        user.setId(11);
+        user.setName("sweet");
+        //以json 的方式来实现
+//        userReadAndWrite.writePeopleInJson(key,user);
+//        User userFromRedis = userReadAndWrite.readPeopleFromJson(key);
+//        Assert.assertEquals(11,userFromRedis.getId().intValue());
+//        Assert.assertEquals("sweet",userFromRedis.getName());
+        userReadAndWrite.writeUserInHash(key,user);
+        Integer id = userReadAndWrite.readUserInHash(key, "id", Integer.class);
+        String name = userReadAndWrite.readUserInHash(key, "name", String.class);
+        Assert.assertEquals(11,id.intValue());
+        Assert.assertEquals("sweet",name);
+        userReadAndWrite.commonRedissionClient.close();
     }
 }
--- src/main/java/com/redisStudy/client/CommonRedissionClient.java
+++ src/main/java/com/redisStudy/client/CommonRedissionClient.java
@@ -2,9 +2,12 @@ package com.redisStudy.client;
 
 import org.redisson.Redisson;
 import org.redisson.api.RBucket;
+import org.redisson.api.RMap;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 
+import java.io.Closeable;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
@@ -12,8 +15,10 @@ import java.util.concurrent.TimeUnit;
 /**
  * 通用的redis java client
  * 使用{@link RedissonClient}
+ *
+ * @author chenwu on 2020.5.5
  */
-public class CommonRedissionClient {
+public class CommonRedissionClient implements Closeable {
 
     public static CommonRedissionClient redissionClientInstance;
     private RedissonClient redissionClient;
@@ -27,6 +32,15 @@ public class CommonRedissionClient {
         Config config = new Config();
         config.useSingleServer().setAddress(redisHostAndPort);
         redissionClient = Redisson.create(config);
+    }
+
+    @Override
+    public void close() {
+        try{
+            redissionClient.shutdown();
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
 
     }
 
@@ -56,11 +70,65 @@ public class CommonRedissionClient {
      * @param timeUnit
      * @author chenwu on 2020.5.5
      */
-    public void setSingleKey(String key, String value, long expireTime, TimeUnit timeUnit){
-        RBucket<Object> bucket = redissionClient.getBucket(key);
+    public <T> void setSingleKey(String key, T value, long expireTime, TimeUnit timeUnit,Class<T> valueClass){
+        RBucket<T> bucket = redissionClient.getBucket(key);
         bucket.set(value);
         if(expireTime>0){
             bucket.expire(expireTime,timeUnit);
         }
+    }
+
+    /**
+     * 从单个key中取值
+     *
+     * @param key
+     * @param <T>
+     * @return T
+     * @author chenwu on 2020.5.5
+     */
+    public <T> T getSingleKey(String key,Class<T> valueClass){
+        RBucket<T> bucket = redissionClient.getBucket(key);
+        if(bucket==null || !bucket.isExists() ){
+            return null;
+        }
+        return bucket.get();
+    }
+
+    /**
+     * hset key
+     *
+     * @param key
+     * @param field
+     * @param value
+     * @param ttlTime
+     * @param timeUnit
+     * @author chenwu on 2020.5.5
+     */
+    public void hsetSingleKey(String key,String field,Object value,long ttlTime,TimeUnit timeUnit){
+        RMap<String, Object> map = redissionClient.getMap(key);
+        boolean isNewKey = false;
+        if(map==null || map.isEmpty()){
+            isNewKey = true;
+        }
+        map.put(field,value);
+        if(isNewKey && ttlTime>0l){
+            map.expire(ttlTime,timeUnit);
+        }
+    }
+
+    /**
+     * hget key
+     *
+     * @param key
+     * @param field
+     * @param valueClass
+     * @param <T>
+     * @return
+     * @author chenwu on 2020.5.5
+     */
+    public <T> T hgetSingleKey(String key,String field,Class<T> valueClass){
+        RMap<String, T> map = redissionClient.getMap(key);
+        T value = map.get(field);
+        return value;
     }
 }
