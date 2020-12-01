bca912609e13d3c047de1f3e47174044cc49c810
chenwu <chenwu@meitunmama.com>
2020-12-01 17:05:30 +0800

    提交leetcode0206 判断链表是否是回文链表

--- src/test/java/nowCode/medium/LeetCode0206.java
+++ src/test/java/nowCode/medium/LeetCode0206.java
@@ -1,4 +1,4 @@
-package leetCode.medium;
+package nowCode.medium;
 
 import leetCode.ListNode;
 
@@ -36,22 +36,32 @@ public class LeetCode0206 {
         ListNode slow = head;
         ListNode fast = head;
         //使用快慢指针找到链表中间节点
-        while(fast!=null && fast.next!=null && fast.next.next!=null){
+        while(fast!=null && fast.next!=null && fast.next.next != null){
             slow = slow.next;
             fast = fast.next.next;
         }
         //反转中间节点以后的链表
         ListNode halfNode = reverseNode(slow.next);
-        while(halfNode!=null && head != null && head.val == halfNode.val){
+        while(halfNode!=null  && head.val == halfNode.val){
             halfNode = halfNode.next;
             head = head.next;
         }
-        return head == null && halfNode==null;
+        return halfNode == null;
     }
 
     public static void main(String[] args) {
         ListNode root = new ListNode(1);
+        ListNode p1 = new ListNode(2);
+        ListNode p2 = new ListNode(3);
+        ListNode p3 = new ListNode(2);
+        ListNode p4 = new ListNode(1);
+        root.next = p1;
+        p1.next = p2;
+        p2.next = p3;
+        p3.next = p4;
         LeetCode0206 leetCode0206 = new LeetCode0206();
+        boolean palindrome = leetCode0206.isPalindrome(root);
+        System.out.println("palindrome:"+palindrome);
     }
 
 }
