bf9ca689ad94e041a1f0ef290091ef3af3fe0392
chenwu <chenwu@meitunmama.com>
2021-01-06 18:22:05 +0800

    提交leetcode897 将二叉树转换为单链表

---
+++ src/test/java/nowCode/medium/LeetCode897.java
@@ -0,0 +1,60 @@
+package nowCode.medium;
+
+import leetCode.TreeNode;
+
+//给你一个树，请你 按中序遍历 重新排列树，使树中最左边的结点现在是树的根，并且每个结点没有左子结点，只有一个右子结点。
+//
+//
+//
+// 示例 ：
+//
+// 输入：[5,3,6,2,4,null,8,1,null,null,null,7,9]
+//
+//       5
+//      / \
+//    3    6
+//   / \    \
+//  2   4    8
+// /        / \
+//1        7   9
+//
+//输出：[1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
+//
+// 1
+//  \
+//   2
+//    \
+//     3
+//      \
+//       4
+//        \
+//         5
+//          \
+//           6
+//            \
+//             7
+//              \
+//               8
+//                \
+//                 9
+public class LeetCode897 {
+
+    private TreeNode resultNode = new TreeNode(0);
+    private TreeNode cur = resultNode;
+
+    private void helpIncreasingBST(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        helpIncreasingBST(root.left);
+        root.left = null;
+        cur.right = root;
+        cur = root;
+        helpIncreasingBST(root.right);
+    }
+
+    public TreeNode increasingBST(TreeNode root) {
+        helpIncreasingBST(root);
+        return resultNode.right;
+    }
+}
