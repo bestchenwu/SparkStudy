648ffc2edfb7e377a113f9ea06f9563eb94a7336
chenwu <chenwu@meitunmama.com>
2021-05-07 18:56:56 +0800

    leetcode71 简化unix路径  ,leetcode401 二进制表

--- src/test/java/leetCode/simple/LeetCode401.java
+++ src/test/java/leetCode/simple/LeetCode401.java
@@ -1,5 +1,6 @@
 package leetCode.simple;
 
+import java.util.ArrayList;
 import java.util.List;
 
 //二进制手表顶部有 4 个 LED 代表 小时（0-11），底部的 6 个 LED 代表 分钟（0-59）。
@@ -33,9 +34,35 @@ import java.util.List;
 // 小时不会以零开头，比如 “01:00” 是不允许的，应为 “1:00”。
 // 分钟必须由两位数组成，可能会以零开头，比如 “10:2” 是无效的，应为 “10:02”。
 // 超过表示范围（小时 0-11，分钟 0-59）的数据将会被舍弃，也就是说不会出现 "13:00", "0:61" 等时间。
+//参考https://blog.csdn.net/weixin_43675940/article/details/106793212
 public class LeetCode401 {
 
     public List<String> readBinaryWatch(int turnedOn) {
-        return null;
+        List<String> list = new ArrayList<>();
+        for(int i = 0;i<12;i++){
+            for(int j = 0;j<60;j++){
+                if(count(i)+count(j)==turnedOn){
+                    list.add(i+":"+(j<10?"0"+j:j));
+                }
+            }
+        }
+        return list;
+    }
+
+    /**
+     * 计算数字n有几个1
+     *
+     * @param n
+     * @return
+     */
+    private int count(int n){
+        int count = 0;
+        while(n>0){
+            if(n%2==1){
+                count+=1;
+            }
+            n = n/2;
+        }
+        return count;
     }
 }
