3cc0fc110807ee155bc354fde6fc5436b4ffd970
chenwu <chenwu@meitunmama.com>
2021-08-10 18:56:19 +0800

    提交leetcode707 设计链表

--- src/test/java/leetCode/medium/LeetCode707.java
+++ src/test/java/leetCode/medium/LeetCode707.java
@@ -26,21 +26,32 @@ package leetCode.medium;
 //linkedList.deleteAtIndex(1);  //现在链表是1-> 3
 //linkedList.get(1);            //返回3
 
+import org.junit.Test;
+
 public class LeetCode707 {
 
-    class Node{
+    class Node {
         private int val;
         private Node next;
         private Node prev;
 
-        public Node(int val){
+        public Node(int val) {
             this.val = val;
         }
 
-        public Node(int val,Node next,Node prev){
+        public Node(int val, Node next, Node prev) {
             this.val = val;
             this.next = next;
             this.prev = prev;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "val=" + val +
+                    ", next=" + next +
+                    //", prev=" + prev +
+                    '}';
         }
     }
 
@@ -48,7 +59,9 @@ public class LeetCode707 {
     private Node tail;
     private int size;
 
-    /** Initialize your data structure here. */
+    /**
+     * Initialize your data structure here.
+     */
     public LeetCode707() {
         head = new Node(0);
         tail = new Node(0);
@@ -57,48 +70,61 @@ public class LeetCode707 {
         size = 0;
     }
 
-    /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+    /**
+     * Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+     */
     public int get(int index) {
-        if(index>=size || index<0){
+        if (index >= size || index < 0) {
             return -1;
         }
         Node tmp = head;
-        for(int i = 0;i<index;i++){
+        for (int i = 0; i <=index; i++) {
             tmp = tmp.next;
         }
         return tmp.val;
     }
 
-    /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
+    /**
+     * Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+     */
     public void addAtHead(int val) {
         Node newNode = new Node(val);
         newNode.next = head.next;
         head.next.prev = newNode;
         newNode.prev = head;
         head.next = newNode;
-        size+=1;
+        size += 1;
     }
 
-    /** Append a node of value val to the last element of the linked list. */
+    /**
+     * Append a node of value val to the last element of the linked list.
+     */
     public void addAtTail(int val) {
         Node newNode = new Node(val);
         newNode.next = tail;
         newNode.prev = tail.prev;
         tail.prev.next = newNode;
         tail.prev = newNode;
-        size+=1;
+        size += 1;
     }
 
-    /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+    @Override
+    public String toString() {
+        return head.next.toString();
+    }
+
+    /**
+     * Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+     */
     public void addAtIndex(int index, int val) {
-        if(index<=0){
+        if (index <= 0) {
             addAtHead(val);
-        }else if(index == size){
+        } else if (index == size) {
             addAtTail(val);
-        }else{
+        } else {
             Node pre = head;
             Node tmp = head.next;
-            for(int i = 1;i<index;i++){
+            for (int i = 1; i <=index; i++) {
                 pre = tmp;
                 tmp = tmp.next;
             }
@@ -107,23 +133,54 @@ public class LeetCode707 {
             newNode.prev = pre;
             tmp.prev = newNode;
             newNode.next = tmp;
-            size+=1;
+            size += 1;
         }
+
+
 
     }
 
-    /** Delete the index-th node in the linked list, if the index is valid. */
+    /**
+     * Delete the index-th node in the linked list, if the index is valid.
+     */
     public void deleteAtIndex(int index) {
-        if(index<0||index>=size){
+        if (index < 0 || index >= size) {
             return;
         }
         Node pre = head;
         Node tmp = head;
-        for(int i = 0;i<index;i++){
+        for (int i = 0; i <=index; i++) {
             pre = tmp;
             tmp = tmp.next;
         }
         pre.next = tmp.next;
         tmp.next.prev = pre;
+        this.size-=1;
+    }
+
+    @Test
+    public void testLeetCode707() {
+        LeetCode707 linkedList = new LeetCode707();
+//        linkedList.addAtHead(1);
+//        linkedList.addAtTail(3);
+//        linkedList.addAtIndex(1, 2);   //链表变为1-> 2-> 3
+//        System.out.println(linkedList);
+//        int value = linkedList.get(1);            //返回2
+//        System.out.println("value="+value);
+//        linkedList.deleteAtIndex(1);  //现在链表是1-> 3
+//        System.out.println(linkedList);
+//        value = linkedList.get(1);            //返回3
+//        System.out.println("value="+value);
+        linkedList.addAtHead(9);
+        int value = linkedList.get(1);
+        linkedList.addAtIndex(1,1 );
+        linkedList.addAtIndex(1,7 );
+        linkedList.deleteAtIndex(1);
+        linkedList.addAtHead(7);
+        linkedList.addAtHead(4);
+        linkedList.deleteAtIndex(1);
+        linkedList.addAtIndex(1, 4);
+        linkedList.addAtHead(2);
+        linkedList.deleteAtIndex(5);
     }
 }
