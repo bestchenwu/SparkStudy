fa9dd097758ce3f75c82eaaa64457cd3ac81f73e
chenwu <chenwu@meitunmama.com>
2023-08-27 19:30:03 +0800

    提交lastRemaining

--- src/test/java/offer/simple/Offer62.java
+++ src/test/java/offer/simple/Offer62.java
@@ -15,8 +15,61 @@ package offer.simple;
  *
  * 输入: n = 10, m = 17
  * 输出: 2
+ *
+ * 1 <= n <= 10^5
+ * 1 <= m <= 10^6
  */
 public class Offer62 {
 
 
+    public int lastRemaining2(int n, int m) {
+        return helpDelete(n,m);
+    }
+
+    public int helpDelete(int n,int m){
+        if(n==1){
+            return 0;
+        }
+        int x = helpDelete(n-1,m);
+        return (m+x)%n;
+    }
+
+    public int lastRemaining(int n, int m) {
+        boolean[] flags = new boolean[n];
+        int step = m;
+        int moveCount = n;
+        int start = 0;
+        helpDelete(flags,step,start,n,moveCount);
+        for(int i = 0;i<n;i++){
+            if(!flags[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private void helpDelete(boolean[] flags,int step,int start,int n,int moveCount){
+        int trueStep = 1,last = start;
+        while(trueStep<=step){
+            if(!flags[start]){
+                trueStep+=1;
+            }
+            last = start;
+            start = (start+1)%n;
+        }
+        flags[last] = true;
+        moveCount-=1;
+        if(moveCount==1){
+            return;
+        }else{
+            helpDelete(flags,step,start,n,moveCount);
+        }
+    }
+
+    public static void main(String[] args) {
+        Offer62 offer62 = new Offer62();
+        int n = 70866,m = 116922;
+        int last = offer62.lastRemaining(n, m);
+        System.out.println(last);
+    }
 }
